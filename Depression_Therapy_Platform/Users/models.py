bf14c8f7b7c0416a43f6650deb95981ceb655fea
Pratik Jallan <pratikjalan11@gmail.com>
2022-09-25 16:35:22 +0530

    base models for all users

--- Depression_Therapy_Platform/Users/models.py
+++ Depression_Therapy_Platform/Users/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 
+
 class CustomUserManager(BaseUserManager):
     use_in_migrations = True
     def create_user(self, email, password, **extra_fields):
@@ -34,12 +35,44 @@ class CustomUserManager(BaseUserManager):
         user.save()
         return user
 
+
 class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(validators=[MinValueValidator(18),MaxValueValidator(100)],null=True,blank=True)
+    gender = models.CharField(max_length=10,choices=(('M', 'Male'), ('F', 'Female'), ('NB', 'Non-Binary')),null=True,blank=True)
     email = models.EmailField(_('email address'), unique=True)
+    phone_number = models.CharField(max_length=10,null=True,blank=True) 
     is_superuser = models.BooleanField(_('superuser status'), default=False)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
+    username = 'first_name' + 'last_name'
     objects = CustomUserManager()
 
     def _str_(self):
-        return self.email
+        return self.user.first_name + self.user.last_name
+
+
+class Patient(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, primary_key=True)
+    past_diseases = models.CharField(max_length=1000,blank=True,null=True)
+    past_medication = models.CharField(max_length=1000,blank=True,null=True)
+
+    def __str__(self):
+        return self.user.first_name + self.user.last_name
+
+class Doctor(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, primary_key=True)
+    qualifications = models.CharField(max_length=1000,blank=True,null=True)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.first_name + self.user.last_name
+
+
+class Sponsor(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, primary_key=True)
+    qualifications = models.CharField(max_length=1000,blank=True,null=True)
+    organization_name = models.CharField(max_length=100,blank=True,null=True)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.first_name + self.user.last_name
