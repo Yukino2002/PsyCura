cd3c0fb9e97e732b37c74ccfdc783ef5d9f607cf
Pratik Jallan <pratikjalan11@gmail.com>
2022-09-27 15:59:24 +0530

    made wrapper, home about contact

--- Depression_Therapy_Platform/Depression_Therapy_Platform/settings.py
+++ Depression_Therapy_Platform/Depression_Therapy_Platform/settings.py
@@ -122,11 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-
-#ADDED MANUALLY
+import os
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
