f56abca0bb2be8924d9c50d531bd5574a7344ee3
Pratik Jallan <pratikjalan11@gmail.com>
2022-10-07 23:03:09 +0530

    doctor verification on staff dashboard complete

--- Depression_Therapy_Platform/Users/urls.py
+++ Depression_Therapy_Platform/Users/urls.py
@@ -7,12 +7,13 @@ app_name = 'Users'
 
 
 urlpatterns = [
-    path('p/home/', views.p_home, name='p_home'), 
-    path('d/home/', views.d_home, name='d_home'), 
-    path('s/home/', views.s_home, name='s_home'), 
-    path('home/', views.home, name='home'), 
-    path('d/pending/', views.doctors_pending, name='doctors_pending'), 
-    path('d/approved/', views.doctors_approved, name='doctors_approved'), 
-    path('d/banned/', views.doctors_banned, name='doctors_banned'), 
-    path('sign-out/', views.sign_out, name='sign_out'), 
+    path('p/home', views.p_home, name='p_home'), 
+    path('d/home', views.d_home, name='d_home'), 
+    path('s/home', views.s_home, name='s_home'), 
+    path('home', views.home, name='home'), 
+    path('d-pending', views.doctors_pending, name='doctors_pending'), 
+    path('d-approved', views.doctors_approved, name='doctors_approved'), 
+    path('d-banned', views.doctors_banned, name='doctors_banned'), 
+    path('d-update/<d_id>', views.doctor_update, name='doctor_update'), 
+    path('sign-out', views.sign_out, name='sign_out'), 
 ]
