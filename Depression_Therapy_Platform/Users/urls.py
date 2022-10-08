9f42cc3c25951c276e659ce9be18a896452629a1
Pratik Jallan <pratikjalan11@gmail.com>
2022-10-08 13:05:42 +0530

    draft staff dashboard completed

--- Depression_Therapy_Platform/Users/urls.py
+++ Depression_Therapy_Platform/Users/urls.py
@@ -7,13 +7,17 @@ app_name = 'Users'
 
 
 urlpatterns = [
-    path('p/home', views.p_home, name='p_home'), 
-    path('d/home', views.d_home, name='d_home'), 
-    path('s/home', views.s_home, name='s_home'), 
-    path('home', views.home, name='home'), 
+    path('p/profile', views.p_home, name='p_home'), 
+    path('d/profile', views.d_home, name='d_home'), 
+    path('s/profile', views.s_home, name='s_home'), 
+    path('profile', views.home, name='home'), 
     path('d-pending', views.doctors_pending, name='doctors_pending'), 
     path('d-approved', views.doctors_approved, name='doctors_approved'), 
     path('d-banned', views.doctors_banned, name='doctors_banned'), 
     path('d-update/<d_id>', views.doctor_update, name='doctor_update'), 
+    path('s-pending', views.sponsors_pending, name='sponsors_pending'), 
+    path('s-approved', views.sponsors_approved, name='sponsors_approved'), 
+    path('s-banned', views.sponsors_banned, name='sponsors_banned'), 
+    path('s-update/<s_id>', views.sponsors_update, name='sponsor_update'), 
     path('sign-out', views.sign_out, name='sign_out'), 
 ]
