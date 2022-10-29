75fe0992d7cad7d0f246065fd5d664536f47fd2b
Pratik <pratikjalan11@gmail.com>
2022-10-29 10:16:05 +0530

    corrected sign up form

--- Depression_Therapy_Platform/Users/models.py
+++ Depression_Therapy_Platform/Users/models.py
@@ -1,18 +1,19 @@
+from django.core.validators import RegexValidator, MaxValueValidator, MinValueValidator
+from datetime import datetime
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from Services.models import Forum
 from email.policy import default
 from django.db import models
 import sys
 sys.path.append("..")
-from Services.models import Forum
-from django.core.validators import RegexValidator,MaxValueValidator,MinValueValidator
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.base_user import BaseUserManager
-from django.utils.translation import gettext_lazy as _
-from django.contrib.auth import get_user_model
-from datetime import datetime
 
 
 class CustomUserManager(BaseUserManager):
     use_in_migrations = True
+
     def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError('Users must have an email address')
@@ -25,7 +26,6 @@ class CustomUserManager(BaseUserManager):
         user.set_password(password)
         user.save()
         return user
-
 
     def create_superuser(self, email, password, **extra_fields):
         if not email:
@@ -42,10 +42,12 @@ class CustomUserManager(BaseUserManager):
 
 
 class CustomUser(AbstractUser):
-    age = models.PositiveIntegerField(validators=[MinValueValidator(18),MaxValueValidator(100)],null=True,blank=True)
-    gender = models.CharField(max_length=10,choices=(('M', 'Male'), ('F', 'Female'), ('NB', 'Non-Binary')),null=True,blank=True)
+    age = models.PositiveIntegerField(validators=[MinValueValidator(
+        18), MaxValueValidator(100)], null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=(
+        ('M', 'Male'), ('F', 'Female'), ('NB', 'Non-Binary')), null=True, blank=True)
     email = models.EmailField(_('email address'), unique=True)
-    phone_number = models.CharField(max_length=10,null=True,blank=True) 
+    phone_number = models.CharField(max_length=10, null=True, blank=True)
     is_superuser = models.BooleanField(_('superuser status'), default=False)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
@@ -57,78 +59,59 @@ class CustomUser(AbstractUser):
 
 
 class Patient(models.Model):
-    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(
+        get_user_model(), on_delete=models.CASCADE, primary_key=True)
     wallet_balance = models.PositiveIntegerField(default=0)
-    past_diseases = models.CharField(max_length=1000,blank=True,null=True)
+    past_diseases = models.CharField(max_length=1000, blank=True, null=True)
     past_medication = models.FileField()
 
-    forum = models.ForeignKey(Forum, on_delete=models.CASCADE, null=True, blank=True)
+    forum = models.ForeignKey(
+        Forum, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.user.first_name + ' ' + self.user.last_name
-    
-    # def book_appointment(self,date,time,doctor):
-    #     present_appointments = Appointment.objects.filter(date=date,time=time,doctor=doctor)
-    #     if present_appointments is None:
-    #         appointment = Appointment(date=date,time=time,patient=self,doctor=doctor)
-    #         appointment.save()
-    #         return "Appointment booked"
-    #     else:
-    #         return "Slot unavailable"
-    
-    # def make_payment(self,amount,doctor):
-    #     DT = datetime.now()
-    #     date = DT.date()
-    #     time = DT.time()
-
-    #     new_transaction = Transaction(date = date,
-    #                                   time = time,
-    #                                   amount = amount,
-    #                                   patient = self,
-    #                                   doctor = doctor)
-        
-    #     new_transaction.save()
-    #     new_transaction.transfer()
-        
-
 
 
 class Doctor(models.Model):
-    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(
+        get_user_model(), on_delete=models.CASCADE, primary_key=True)
     wallet_balance = models.PositiveIntegerField(default=0)
-    qualifications = models.CharField(max_length=1000,blank=True,null=True)
-    certificate = models.FileField(default = None)
-    specialization = models.CharField(default = 'a',max_length=30,choices=(("abnormal","abnormal"),
-                                                              ("biopsychologist","biopsychologist"),
-                                                              ("cognitive","cognitive"),
-                                                              ("developmental","developmental"),
-                                                              ("personality","personality"),
-                                                              ("forensic","forensic"),
-                                                              ("industrial-organizational","industrial-organizational")))
-    experience = models.PositiveIntegerField(default = 0)
-    is_approved = models.CharField(max_length=10,choices=(('A', 'Approved'), ('P', 'Pending'), ('B', 'Banned')),default='P')
-    
-
+    qualifications = models.CharField(max_length=1000, blank=True, null=True)
+    certificate = models.FileField(default=None)
+    specialization = models.CharField(default='a', max_length=30, choices=(("Abnormal", "Abnormal"),
+                                                                           ("Biopsychologist",
+                                                                            "Biopsychologist"),
+                                                                           ("Cognitive",
+                                                                            "Cognitive"),
+                                                                           ("Developmental",
+                                                                            "Developmental"),
+                                                                           ("Personality",
+                                                                            "Personality"),
+                                                                           ("Forensic",
+                                                                            "Forensic"),
+                                                                           ("Industrial-organizational",
+                                                                           "Industrial-organizational")
+                                                                           )
+                                      )
+    experience = models.PositiveIntegerField(default=0, blank=True, null=True)
+    is_approved = models.CharField(max_length=10, choices=(
+        ('A', 'Approved'), ('P', 'Pending'), ('B', 'Banned')), default='P')
 
     def __str__(self):
         return self.user.first_name + ' ' + self.user.last_name
 
-    # def set_time_slot(self,day,time):
-    #     slot = Time_Table(day = day,time = time,doctor = self)
-    #     slot.save()
-    
-    # def delete_time_slot(self,day,time):
-    #     Time_Table.objects.filter(day=day,time=time,doctor=self).delete()
-
 
 class Sponsor(models.Model):
-    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, primary_key=True)
-    qualifications = models.CharField(max_length=1000,blank=True,null=True)
-    organisation_name = models.CharField(max_length=100,blank=True,null=True)
+    user = models.OneToOneField(
+        get_user_model(), on_delete=models.CASCADE, primary_key=True)
+    qualifications = models.CharField(max_length=1000, blank=True, null=True)
+    organisation_name = models.CharField(max_length=100, blank=True, null=True)
     certificate = models.FileField()
-    is_approved = models.CharField(max_length=10,choices=(('A', 'Approved'), ('P', 'Pending'), ('B', 'Banned')),default='P')
+    is_approved = models.CharField(max_length=10, choices=(
+        ('A', 'Approved'), ('P', 'Pending'), ('B', 'Banned')), default='P')
 
-    forum = models.ForeignKey(Forum, on_delete=models.CASCADE, null=True, blank=True)
+    forum = models.ForeignKey(
+        Forum, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-        return self.user.first_name + ' ' + self.user.last_name 
+        return self.user.first_name + ' ' + self.user.last_name
--- Depression_Therapy_Platform/Users/views.py
+++ Depression_Therapy_Platform/Users/views.py
@@ -8,10 +8,16 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 
 
+
+
+
+
 @login_required(login_url='sign_in')
 @allowed_users(allowed_users=['patient'])
 def p_home(request):
-    return render(request, 'Users/patient/home.html',{'patient':request.user})
+    patient = Patient.objects.get(user=request.user)
+    return render(request, 'Users/patient/profile/profile.html', {'patient':patient})
+
 
 @login_required(login_url='sign_in')
 @allowed_users(allowed_users=['patient'])
@@ -26,18 +32,15 @@ def appointments(request):
 @login_required(login_url='sign_in')
 @allowed_users(allowed_users=['patient'])
 def p_doctors(request):
-    # if request.method == "GET":
-    #     specialization = request.GET.get('specialization')
-    #     experience = request.GET.get('experiance')
-    #     doctors = Doctor.objects.all().filter(is_approved='A',specialization=specialization,experience=experience)
-    # else:
+    patient = Patient.objects.get(user=request.user)
     if request.method == "POST":
         specialization = request.POST.get("specialization")
-        experiance = request.POST.get("experience")
-        doctors = Doctor.objects.all().filter(specialization = specialization,experiance = experiance)
+        experience = request.POST.get("experience")
+        doctors = Doctor.objects.all().filter(specialization = specialization, experience = experience)
     else:
         doctors = Doctor.objects.all()
-    return render(request,"Users/patient/p_doctors.html",{'doctors':doctors})
+
+    return render(request,"Users/patient/doctors/p_doctors.html", {'patient':patient, 'doctors':doctors})
 
 
 @login_required(login_url='sign_in')
@@ -67,20 +70,56 @@ def wallet(request):
             else:
                 mssg = "Invalid amount. Please check the current balance"
         patient.save()
-    return render(request,"Users/patient/wallet.html",{"patient":patient,"mssg":mssg})
+    return render(request,"Users/patient/wallet.html", {"patient":patient,"mssg":mssg})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @login_required(login_url='sign_in')
 @allowed_users(allowed_users=['doctor'])
 def d_home(request):
     doctor = Doctor.objects.get(pk = request.user.id)
-    return render(request, 'Users/doctor/home.html',{'doctor':doctor})
+    return render(request, 'Users/doctor/home.html', {'doctor':doctor})
 
 
 @login_required(login_url='sign_in')
 @allowed_users(allowed_users=['sponsor'])
 def s_home(request):
     return render(request, 'Users/sponsor/home.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @login_required(login_url='sign_in')
